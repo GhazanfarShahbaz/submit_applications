61bb7c8b62976c20680009eb36e6218773ab21b5
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2020-11-27 00:22:21 -0500

    Custom questions

--- submit.py
+++ submit.py
@@ -5,21 +5,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-RESUME = "/Users/ghazshahbaz/documents/projects/submit_applications/Ghazanfar_Shahbaz_Resume.pdf"
-FIRST_NAME = "Ghazanfar" 
-LAST_NAME = 'Shahbaz'
-FULL_NAME = "Ghazanfar Shahbaz"
-EMAIL = 'ghazanfarshahbaz2409@gmail.com'
-SCHOOL_EMAIL = 'ghazanfar.shahbaz95@myhunter.cuny.edu'
-PHONE = '(1)347-493-6743'
-LINKEDIN_URL = 'https://www.linkedin.com/in/GhazanfarShahbaz/'
-GITHUB_URL = 'https://github.com/GhazanfarShahbaz'
-CURRENT_COMPANY = 'UnitedHealth Group'
-ADDITIONAL_INFORMATION = "I am currently a sophomore at Hunter College, majoring in computer science and minoring in math and philosophy. At Hunter I am the core tech lead and treasurer of the Google Student Developer Club as well as a part of the the Thomas Hunters Honors Program. I am currently interning at UnitedHealth Group as a software engineer intern. In my free time i spend time reading books, cooking, and playing videogames."
+RESUME = "THIS SHOULD BE A PATH"
+FIRST_NAME = ""
+LAST_NAME = ""
+FULL_NAME = ""
+EMAIL = ""
+SCHOOL_EMAIL = ""
+PHONE = ""
+LINKEDIN_URL = ""
+GITHUB_URL = ""
+CURRENT_COMPANY = ""
+ADDITIONAL_INFORMATION = ""
 
+
+CUSTOM_QUESTIONS_ANSWERS = {
+    "How did you hear about us?" : "TEST1",
+    "Given the pandemic, what is your internship plan? When are you available to start?" : "TEST2"
+}
+CUSTOM_QUESTIONS = []
 
 browser = webdriver.Chrome()
 browser.get('https://jobs.lever.co/unify/dca0a7bd-41ab-4f24-a1c3-3176520f7437/apply')
+
+for x in browser.find_elements_by_xpath("//input"):
+    print(x.text)
+
+
 browser.find_element_by_xpath('//input[@name = "resume"]').send_keys(RESUME)
 browser.find_element_by_xpath('//input[@name = "name"]').send_keys(FULL_NAME)
 # browser.find_element_by_xpath('//input[@name = "email"]').clear()
@@ -31,3 +42,18 @@ browser.find_element_by_xpath('//input[@name = "urls[GitHub]"]').send_keys(GITHU
 
 browser.find_element_by_xpath('//textarea[@name = "comments"]').send_keys(ADDITIONAL_INFORMATION)
 # browser.find_element_by_xpath('//button[@class = "postings-btn template-btn-submit cerulean"]').click()
+
+for x in browser.find_elements_by_xpath('//div[@class="application-label full-width textarea"]'):
+    val = x.text
+    val = val[0: len(val)-1].strip()
+    CUSTOM_QUESTIONS.append(val)
+
+
+for x,y in enumerate(browser.find_elements_by_xpath('//textarea[@class="card-field-input"]')):
+    # y.send_keys(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
+    q = CUSTOM_QUESTIONS[x]
+    a = CUSTOM_QUESTIONS_ANSWERS[q]
+    y.send_keys(a)
+    # print(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
+
+print(CUSTOM_QUESTIONS)
