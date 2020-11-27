2a811837993bd3f71ddd6e98a2eea36239778509
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2020-11-26 23:47:19 -0500

    Files

--- submit.py
+++ submit.py
@@ -5,17 +5,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-RESUME = "THIS SHOULD BE A PATH"
-FIRST_NAME = ""
-LAST_NAME = ""
-FULL_NAME = ""
-EMAIL = ""
-SCHOOL_EMAIL = ""
-PHONE = ""
-LINKEDIN_URL = ""
-GITHUB_URL = ""
-CURRENT_COMPANY = ""
-ADDITIONAL_INFORMATION = ""
+RESUME = "/Users/ghazshahbaz/documents/projects/submit_applications/Ghazanfar_Shahbaz_Resume.pdf"
+FIRST_NAME = "Ghazanfar" 
+LAST_NAME = 'Shahbaz'
+FULL_NAME = "Ghazanfar Shahbaz"
+EMAIL = 'ghazanfarshahbaz2409@gmail.com'
+SCHOOL_EMAIL = 'ghazanfar.shahbaz95@myhunter.cuny.edu'
+PHONE = '(1)347-493-6743'
+LINKEDIN_URL = 'https://www.linkedin.com/in/GhazanfarShahbaz/'
+GITHUB_URL = 'https://github.com/GhazanfarShahbaz'
+CURRENT_COMPANY = 'UnitedHealth Group'
+ADDITIONAL_INFORMATION = "I am currently a sophomore at Hunter College, majoring in computer science and minoring in math and philosophy. At Hunter I am the core tech lead and treasurer of the Google Student Developer Club as well as a part of the the Thomas Hunters Honors Program. I am currently interning at UnitedHealth Group as a software engineer intern. In my free time i spend time reading books, cooking, and playing videogames."
+
 
 browser = webdriver.Chrome()
 browser.get('https://jobs.lever.co/applied/c22805d5-2006-4867-bb32-671951b17206/apply')
@@ -29,4 +30,4 @@ browser.find_element_by_xpath('//input[@name = "urls[LinkedIn]"]').send_keys(LIN
 browser.find_element_by_xpath('//input[@name = "urls[GitHub]"]').send_keys(GITHUB_URL)
 
 browser.find_element_by_xpath('//textarea[@name = "comments"]').send_keys(ADDITIONAL_INFORMATION)
-browser.find_element_by_xpath('//button[@class = "postings-btn template-btn-submit cerulean"]').click()
+# browser.find_element_by_xpath('//button[@class = "postings-btn template-btn-submit cerulean"]').click()
