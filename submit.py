9c65abbe39910c4e02f873b93675596e53e94a75
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2021-01-05 20:39:08 -0500

    Basic input function

--- submit.py
+++ submit.py
@@ -17,51 +17,59 @@ def processData() -> dict:
     dataFile.close()
     return data
 
-data = processData()
 
-CUSTOM_QUESTIONS_ANSWERS = {
-    "How did you hear about us?" : "TEST1",
-    "Given the pandemic, what is your internship plan? When are you available to start?" : "TEST2"
-}
-CUSTOM_QUESTIONS = []
+def inputBasicData(browser, data: dict) -> None:
+    browser.find_element_by_xpath('//input[@name = "resume"]').send_keys(data['RESUME'])
+    browser.find_element_by_xpath('//input[@name = "name"]').send_keys(data['FULL_NAME'])
+    # browser.find_element_by_xpath('//input[@name = "email"]').clear()
+    browser.find_element_by_xpath('//input[@name = "email"]').send_keys(data['PHONE'])
+    browser.find_element_by_xpath('//input[@name = "phone"]').send_keys(data['PHONE'])
+    browser.find_element_by_xpath('//input[@name = "org"]').send_keys(data['CURRENT_COMPANY'])
+    browser.find_element_by_xpath('//input[@name = "urls[LinkedIn]"]').send_keys(data['LINKEDIN_URL'])
+    browser.find_element_by_xpath('//input[@name = "urls[GitHub]"]').send_keys(data['GITHUB_URL'])
 
-browser = webdriver.Chrome()
-browser.get('https://jobs.lever.co/unify/dca0a7bd-41ab-4f24-a1c3-3176520f7437/apply')
-
-for x in browser.find_elements_by_xpath("//input"):
-    print(x.text)
-
-
-browser.find_element_by_xpath('//input[@name = "resume"]').send_keys(data['RESUME'])
-browser.find_element_by_xpath('//input[@name = "name"]').send_keys(data['FULL_NAME'])
-# browser.find_element_by_xpath('//input[@name = "email"]').clear()
-browser.find_element_by_xpath('//input[@name = "email"]').send_keys(data['PHONE'])
-browser.find_element_by_xpath('//input[@name = "phone"]').send_keys(data['PHONE'])
-browser.find_element_by_xpath('//input[@name = "org"]').send_keys(data['CURRENT_COMPANY'])
-browser.find_element_by_xpath('//input[@name = "urls[LinkedIn]"]').send_keys(data['LINKEDIN_URL'])
-browser.find_element_by_xpath('//input[@name = "urls[GitHub]"]').send_keys(data['GITHUB_URL'])
-
-browser.find_element_by_xpath('//textarea[@name = "comments"]').send_keys(data['ADDITIONAL_INFORMATION'])
-
-#next line is submit
-# browser.find_element_by_xpath('//button[@class = "postings-btn template-btn-submit cerulean"]').click()
-
-"""
-Have to make it so that custom questions ask for input
-"""
+    browser.find_element_by_xpath('//textarea[@name = "comments"]').send_keys(data['ADDITIONAL_INFORMATION'])
 
 
-for x in browser.find_elements_by_xpath('//div[@class="application-label full-width textarea"]'):
-    val = x.text
-    val = val[0: len(val)-1].strip()
-    CUSTOM_QUESTIONS.append(val)
+if __name__ == "__main__":
+
+    data = processData()
+
+    CUSTOM_QUESTIONS_ANSWERS = {
+        "How did you hear about us?" : "TEST1",
+        "Given the pandemic, what is your internship plan? When are you available to start?" : "TEST2"
+    }
+    CUSTOM_QUESTIONS = []
+
+    browser = webdriver.Chrome()
+    browser.get('https://jobs.lever.co/unify/dca0a7bd-41ab-4f24-a1c3-3176520f7437/apply')
+
+    # for x in browser.find_elements_by_xpath("//input"):
+    #     print(x.text)
+
+    inputBasicData(browser, data)
+
+    #next line is submit
+    # browser.find_element_by_xpath('//button[@class = "postings-btn template-btn-submit cerulean"]').click()
+
+    """
+    Have to make it so that custom questions ask for input
+    """
 
 
-for x,y in enumerate(browser.find_elements_by_xpath('//textarea[@class="card-field-input"]')):
-    # y.send_keys(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
-    q = CUSTOM_QUESTIONS[x]
-    a = CUSTOM_QUESTIONS_ANSWERS[q]
-    y.send_keys(a)
-    # print(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
+    for x in browser.find_elements_by_xpath('//div[@class="application-label full-width textarea"]'):
+        val = x.text
+        val = val[0: len(val)-1].strip()
+        CUSTOM_QUESTIONS.append(val)
 
-print(CUSTOM_QUESTIONS)
+
+    for x,y in enumerate(browser.find_elements_by_xpath('//textarea[@class="card-field-input"]')):
+        # y.send_keys(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
+        q = CUSTOM_QUESTIONS[x]
+        a = CUSTOM_QUESTIONS_ANSWERS[q]
+        y.send_keys(a)
+        # print(CUSTOM_QUESTIONS_ANSWERS(CUSTOM_QUESTIONS[x]))
+
+    print(CUSTOM_QUESTIONS)
+
+    browser.close()
